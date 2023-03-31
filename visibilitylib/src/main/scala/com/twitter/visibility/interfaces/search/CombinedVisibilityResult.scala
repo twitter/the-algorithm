f507b9d554ee860c186dfb583352fe047544d4bb
ridoy <majumdar.ridoy@gmail.com>
2023-03-31 19:56:32 -0400

    Improve algorithm

--- visibilitylib/src/main/scala/com/twitter/visibility/interfaces/search/CombinedVisibilityResult.scala
+++
@@ -1,7 +0,0 @@
-package com.twitter.visibility.interfaces.search
-
-import com.twitter.visibility.builder.VisibilityResult
-
-case class CombinedVisibilityResult(
-  tweetVisibilityResult: VisibilityResult,
-  quotedTweetVisibilityResult: Option[VisibilityResult])
