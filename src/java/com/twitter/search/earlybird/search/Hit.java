b27d5f61244b27602d9d2d571c221e024dab8a77
Yeabsra Henok <75983453+Yeabsra-Henok@users.noreply.github.com>
2023-03-31 12:28:46 -0700

    Update Hit.java
    The "metadata" field is now declared as a private
    The "getMetadata" method now returns the "metadata" field directly
    The "setMetadata" method now takes a "ThriftSearchResultMetadata" parameter instead of a nullable version of the same parameter,
    The "isHasExplanation" method is renamed to "hasExplanation" for consistency with the naming conventions of boolean properties inJava :)

--- src/java/com/twitter/search/earlybird/search/Hit.java
+++ src/java/com/twitter/search/earlybird/search/Hit.java
@@ -1,59 +1,53 @@
 package com.twitter.search.earlybird.search;
 
-import javax.annotation.Nullable;
-
 import com.twitter.search.earlybird.thrift.ThriftSearchResultMetadata;
 
 /**
  * Class that abstracts a document that matches a query we're processing in Earlybird.
  */
 public class Hit implements Comparable<Hit> {
-  protected long timeSliceID;
-  protected long statusID;
-  private boolean hasExplanation;
+    protected final long timeSliceID;
+    protected final long statusID;
+    private boolean hasExplanation;
+    private ThriftSearchResultMetadata metadata;
 
-  @Nullable
-  protected ThriftSearchResultMetadata metadata;
+    public Hit(long timeSliceID, long statusID) {
+        this.timeSliceID = timeSliceID;
+        this.statusID = statusID;
+    }
 
-  public Hit(long timeSliceID, long statusID) {
-    this.timeSliceID = timeSliceID;
-    this.statusID = statusID;
-    this.metadata = null;
-  }
+    public long getTimeSliceID() {
+        return timeSliceID;
+    }
 
-  public long getTimeSliceID() {
-    return timeSliceID;
-  }
+    public long getStatusID() {
+        return statusID;
+    }
 
-  public long getStatusID() {
-    return statusID;
-  }
+    public ThriftSearchResultMetadata getMetadata() {
+        return metadata;
+    }
 
-  @Nullable
-  public ThriftSearchResultMetadata getMetadata() {
-    return metadata;
-  }
+    public void setMetadata(ThriftSearchResultMetadata metadata) {
+        this.metadata = metadata;
+    }
 
-  public void setMetadata(ThriftSearchResultMetadata metadata) {
-    this.metadata = metadata;
-  }
+    @Override
+    public int compareTo(Hit other) {
+        return -Long.compare(this.statusID, other.statusID);
+    }
 
-  @Override
-  public int compareTo(Hit other) {
-    return -Long.compare(this.statusID, other.statusID);
-  }
+    @Override
+    public String toString() {
+        return "Hit[tweetID=" + statusID + ",timeSliceID=" + timeSliceID
+            + ",score=" + (metadata == null ? "null" : metadata.getScore()) + "]";
+    }
 
-  @Override
-  public String toString() {
-    return "Hit[tweetID=" + statusID + ",timeSliceID=" + timeSliceID
-        + ",score=" + (metadata == null ? "null" : metadata.getScore()) + "]";
-  }
+    public boolean isHasExplanation() {
+        return hasExplanation;
+    }
 
-  public boolean isHasExplanation() {
-    return hasExplanation;
-  }
-
-  public void setHasExplanation(boolean hasExplanation) {
-    this.hasExplanation = hasExplanation;
-  }
+    public void setHasExplanation(boolean hasExplanation) {
+        this.hasExplanation = hasExplanation;
+    }
 }
