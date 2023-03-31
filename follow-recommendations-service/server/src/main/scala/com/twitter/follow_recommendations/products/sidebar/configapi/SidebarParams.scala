5281609fe810ed65ed96e9aa471bf040142415cd
Owen Lockwood <42878312+lockwo@users.noreply.github.com>
2023-03-31 14:41:52 -0600

    Touch grass

--- follow-recommendations-service/server/src/main/scala/com/twitter/follow_recommendations/products/sidebar/configapi/SidebarParams.scala
+++
@@ -1,9 +0,0 @@
-package com.twitter.follow_recommendations.products.sidebar.configapi
-
-import com.twitter.timelines.configapi.Param
-
-object SidebarParams {
-  object EnableProduct extends Param[Boolean](false)
-
-  object DefaultMaxResults extends Param[Int](20)
-}
