1d03a69d8dae683cfce802e36f3ee93a860d54a7
znwhite <zwhite@twinprimemedia.com>
2023-03-31 15:21:32 -0400

    algorithm edits

--- product-mixer/component-library/src/main/scala/com/twitter/product_mixer/component_library/decorator/urt/builder/richtext/twitter_text/TwitterTextRendererProcessor.scala
+++
@@ -1,5 +0,0 @@
-package com.twitter.product_mixer.component_library.decorator.urt.builder.richtext.twitter_text
-
-trait TwitterTextRendererProcessor {
-  def process(twitterTextRichTextBuilder: TwitterTextRenderer): TwitterTextRenderer
-}
--- product-mixer/core/src/main/scala/com/twitter/product_mixer/core/model/marshalling/response/urt/promoted/DisclosureType.scala
+++
@@ -1,8 +0,0 @@
-package com.twitter.product_mixer.core.model.marshalling.response.urt.promoted
-
-sealed trait DisclosureType
-
-case object NoDisclosure extends DisclosureType
-case object Political extends DisclosureType
-case object Earned extends DisclosureType
-case object Issue extends DisclosureType
