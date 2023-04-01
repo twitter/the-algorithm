870965c68e04888c0cb9ad6da948a4324d431ba4
tornikeo <tornikeonoprishvili@gmail.com>
2023-04-01 09:45:40 +0000

    Fix twitter

--- product-mixer/core/src/main/scala/com/twitter/product_mixer/core/model/marshalling/response/urt/item/tombstone/TombstoneInfo.scala
+++
@@ -1,8 +0,0 @@
-package com.twitter.product_mixer.core.model.marshalling.response.urt.item.tombstone
-
-import com.twitter.product_mixer.core.model.marshalling.response.urt.richtext.RichText
-
-case class TombstoneInfo(
-  text: String,
-  richText: Option[RichText],
-  richRevealText: Option[RichText])
--- product-mixer/core/src/main/scala/com/twitter/product_mixer/core/model/marshalling/response/urt/item/topic/TopicFunctionalityType.scala
+++
@@ -1,7 +0,0 @@
-package com.twitter.product_mixer.core.model.marshalling.response.urt.item.topic
-
-sealed trait TopicFunctionalityType
-
-case object BasicTopicFunctionalityType extends TopicFunctionalityType
-case object PivotTopicFunctionalityType extends TopicFunctionalityType
-case object RecommendationTopicFunctionalityType extends TopicFunctionalityType
