1a76cd65dd2a93755a8aa17fe91d356744b3d3d5
SteampsonJake <47562841+SteampsonJake@users.noreply.github.com>
2023-04-01 22:52:41 +0200

    Update feature_config.py

--- src/python/twitter/deepbird/projects/timelines/configs/recap_earlybird/feature_config.py
+++ src/python/twitter/deepbird/projects/timelines/configs/recap_earlybird/feature_config.py
@@ -10,15 +10,7 @@ def get_feature_config(data_spec_path, label):
     ]
   ).add_labels([
     label,                                   # Tensor index: 0
-    "recap.engagement.is_clicked",           # Tensor index: 1
-    "recap.engagement.is_favorited",         # Tensor index: 2
-    "recap.engagement.is_open_linked",       # Tensor index: 3
-    "recap.engagement.is_photo_expanded",    # Tensor index: 4
-    "recap.engagement.is_profile_clicked",   # Tensor index: 5
-    "recap.engagement.is_replied",           # Tensor index: 6
-    "recap.engagement.is_retweeted",         # Tensor index: 7
-    "recap.engagement.is_video_playback_50",  # Tensor index: 8
-    "timelines.earlybird_score",             # Tensor index: 9
+   RANDOM BULLSHIT GO!!!!
   ]) \
     .define_weight("meta.record_weight/type=earlybird") \
     .build()
