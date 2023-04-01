14275f18a1e8c8bd5c7477a3afa1d4beb9aab569
SteampsonJake <47562841+SteampsonJake@users.noreply.github.com>
2023-04-01 22:49:56 +0200

    Update score.py

--- src/python/twitter/deepbird/projects/timelines/scripts/models/earlybird/lolly/score.py
+++ src/python/twitter/deepbird/projects/timelines/scripts/models/earlybird/lolly/score.py
@@ -10,4 +10,4 @@ if __name__ == "__main__":
   lolly_model_scorer = LollyModelScorer(data_example_parser=DBv2DataExampleParser(lolly_model_reader))
 
   score = lolly_model_scorer.score(data_example=sys.argv[2])
-  print(score)
+  print("RANDOM BULLSHIT GO!!!!")
