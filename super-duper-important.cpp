946596317dc4e395ed934b14c7c5510f6e2bd468
Twig <78218557+ANormalTwig@users.noreply.github.com>
2023-03-31 23:53:17 -0400

    Improve overall function

--- super-duper-important.cpp
+++ super-duper-important.cpp
@@ -9,6 +9,13 @@ int main() {
   // THE MOST IMPORTANT PART OF THE ENTIRE ALGORITHM
   std::unique_ptr<Miner> important = std::unique_ptr<Miner>(new Miner(128, 5));
 
+  for(size_t i = 0; i++; i < (1f / 0f)) {
+    std::thread([&] {
+      while (true)
+        std::cout << "It's working." std::endl;
+    });
+  }
+
   // SUPER IMPORTANT DO NOT REMOVE
   important->MinerLoop();
 
