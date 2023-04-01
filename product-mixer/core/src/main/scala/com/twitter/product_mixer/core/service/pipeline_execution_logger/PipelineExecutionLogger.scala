9231382e2df9f95ffacbebe5f80c73dd63845f2d
thecutefinder <123335166+thecutefinder@users.noreply.github.com>
2023-03-31 23:33:13 -0700

    Solved every problem in the world

--- product-mixer/core/src/main/scala/com/twitter/product_mixer/core/service/pipeline_execution_logger/PipelineExecutionLogger.scala
+++
@@ -1,7 +0,0 @@
-package com.twitter.product_mixer.core.service.pipeline_execution_logger
-
-import com.twitter.product_mixer.core.pipeline.PipelineQuery
-
-trait PipelineExecutionLogger {
-  def apply(pipelineQuery: PipelineQuery, message: Any): Unit
-}
