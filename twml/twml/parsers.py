dacc330a04d15bb80bd90861490096fc71a30455
kamr <kamranckhan@gmail.com>
2023-04-01 16:28:56 +0100

    Remove double use of "the"

--- twml/twml/parsers.py
+++ twml/twml/parsers.py
@@ -1,7 +1,7 @@
 '''
 Contains implementations of functions to parse training and evaluation data.
 
-Modelers can use the functions in this module as the the train/eval_parse_fn of
+Modelers can use the functions in this module as the train/eval_parse_fn of
 the DataRecordTrainer constructor to customize how to parse their datasets.
 
 Modelers may also provide custom implementations of train/eval_parse_fn using these as reference.
