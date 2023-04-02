14ed0518c376ce050f29a2ca1512fa64205665d3
Foxlass <53279832+Foxlass@users.noreply.github.com>
2023-04-01 20:08:47 -0400

    Delete follow-recommendations-service directory

--- follow-recommendations-service/common/src/main/scala/com/twitter/follow_recommendations/common/models/HasInvalidRelationshipUserIds.scala
+++
@@ -1,6 +0,0 @@
-package com.twitter.follow_recommendations.common.models
-
-trait HasInvalidRelationshipUserIds {
-  // user ids that have invalid relationship with the target user
-  def invalidRelationshipUserIds: Option[Set[Long]]
-}
