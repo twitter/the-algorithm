eabae11589205e441fac9acddd5fcc950605340d
Daniel Haim <2808046+danielhaim1@users.noreply.github.com>
2023-04-03 10:14:41 +0200

    TensorFlow protocol & bash script improvement
    1. The update proposes improvements to a Bash script that runs a Rust application with multiple command line arguments. The suggested changes include adding comments, using variables for each argument, using double quotes for variables, using descriptive variable names, and adding a shebang for the Bash shell specifically. An updated script is provided as an example.
    
    2. The protocol buffer file with version information for serialized data used in TensorFlow now defines a VersionDef message with fields for the producer version, minimum consumer version, and a list of disallowed consumer versions.

--- navi/navi/scripts/run_onnx.sh
+++ navi/navi/scripts/run_onnx.sh
@@ -1,10 +1,28 @@
-#!/bin/sh
-#RUST_LOG=debug LD_LIBRARY_PATH=so/onnx/lib target/release/navi_onnx --port 30 --num-worker-threads 8 --intra-op-parallelism 8 --inter-op-parallelism 8 \
-RUST_LOG=info LD_LIBRARY_PATH=so/onnx/lib cargo run --bin navi_onnx --features onnx -- \
-    --port 30 --num-worker-threads 8 --intra-op-parallelism 8 --inter-op-parallelism 8 \
-    --model-check-interval-secs 30 \
-    --model-dir models/int8 \
-    --output caligrated_probabilities \
-    --input "" \
-    --modelsync-cli "echo" \
-    --onnx-ep-options use_arena=true
+# This script runs the navi_onnx Rust application with command line arguments.
+
+# Set variables for command line arguments
+LOG_LEVEL="info"
+LIB_PATH="so/onnx/lib"
+PORT="30"
+NUM_WORKER_THREADS="8"
+INTRA_OP_PARALLELISM="8"
+INTER_OP_PARALLELISM="8"
+MODEL_CHECK_INTERVAL_SECS="30"
+MODEL_DIR="models/int8"
+OUTPUT="calibrated_probabilities"
+INPUT=""
+MODEL_SYNC_CLI="echo"
+ONNX_EP_OPTIONS="use_arena=true"
+
+# Run the navi_onnx application
+RUST_LOG="$LOG_LEVEL" LD_LIBRARY_PATH="$LIB_PATH" cargo run --bin navi_onnx --features onnx -- \
+    --port "$PORT" \
+    --num-worker-threads "$NUM_WORKER_THREADS" \
+    --intra-op-parallelism "$INTRA_OP_PARALLELISM" \
+    --inter-op-parallelism "$INTER_OP_PARALLELISM" \
+    --model-check-interval-secs "$MODEL_CHECK_INTERVAL_SECS" \
+    --model-dir "$MODEL_DIR" \
+    --output "$OUTPUT" \
+    --input "$INPUT" \
+    --modelsync-cli "$MODEL_SYNC_CLI" \
+    --onnx-ep-options "$ONNX_EP_OPTIONS"
