834d0533c501c1dd2d32300e41553a18f84a84a2
Daniel Haim <2808046+danielhaim1@users.noreply.github.com>
2023-04-03 10:05:33 +0200

    Update SpaceSafetyLabelType.scala
    The updated code has removed the UkraineCrisisTopic case object from the list of safety label types. Other than that, there are no significant changes to the code structure, organization, or functionality.
    
    This update relates to https://github.com/twitter/the-algorithm/issues/1489

--- visibilitylib/src/main/scala/com/twitter/visibility/models/SpaceSafetyLabelType.scala
+++ visibilitylib/src/main/scala/com/twitter/visibility/models/SpaceSafetyLabelType.scala
@@ -36,7 +36,6 @@ object SpaceSafetyLabelType extends SafetyLabelType {
     s.SpaceSafetyLabelType.HatefulHighRecall -> HatefulHighRecall,
     s.SpaceSafetyLabelType.ViolenceHighRecall -> ViolenceHighRecall,
     s.SpaceSafetyLabelType.HighToxicityModelScore -> HighToxicityModelScore,
-    s.SpaceSafetyLabelType.UkraineCrisisTopic -> UkraineCrisisTopic,
     s.SpaceSafetyLabelType.DoNotPublicPublish -> DoNotPublicPublish,
     s.SpaceSafetyLabelType.Reserved16 -> Deprecated,
     s.SpaceSafetyLabelType.Reserved17 -> Deprecated,
@@ -68,9 +67,6 @@ object SpaceSafetyLabelType extends SafetyLabelType {
   case object HatefulHighRecall extends SpaceSafetyLabelType
   case object ViolenceHighRecall extends SpaceSafetyLabelType
   case object HighToxicityModelScore extends SpaceSafetyLabelType
-
-  case object UkraineCrisisTopic extends SpaceSafetyLabelType
-
   case object DoNotPublicPublish extends SpaceSafetyLabelType
 
   case object Deprecated extends SpaceSafetyLabelType
