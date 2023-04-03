ca966624555a9a8c986c7b0032adb28cc3f8edef
justinlime <justinlime1999@gmail.com>
2023-04-03 06:29:04 -0500

    Subscribe to virbox fr

--- follow-recommendations-service/common/src/main/scala/com/twitter/follow_recommendations/common/models/GeohashAndCountryCode.scala
+++
@@ -1,3 +0,0 @@
-package com.twitter.follow_recommendations.common.models
-
-case class GeohashAndCountryCode(geohash: Option[String], countryCode: Option[String])
--- product-mixer/core/src/main/scala/com/twitter/product_mixer/core/model/marshalling/response/urt/metadata/ReplyPinState.scala
+++
@@ -1,7 +0,0 @@
-package com.twitter.product_mixer.core.model.marshalling.response.urt.metadata
-
-sealed trait ReplyPinState
-
-object PinnedReplyPinState extends ReplyPinState
-object PinnableReplyPinState extends ReplyPinState
-object NotPinnableReplyPinState extends ReplyPinState
