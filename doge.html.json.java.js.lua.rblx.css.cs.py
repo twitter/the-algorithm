8da97195870f8f6db6098f30429c56739c2db7a4
GAMBLING-MAN <114881900+GAMBLING-MAN@users.noreply.github.com>
2023-04-04 15:02:13 -0400

    Create doge.html.json.java.js.lua.rblx.css.cs.py
    this is a good file and should be added immediately

---
+++ doge.html.json.java.js.lua.rblx.css.cs.py
@@ -0,0 +1,13 @@
+#i don't know python but i think this achieves your goals
+#also this file isn't run unless you have a much bigger problem on the backend than imaginable
+
+
+
+
+for in in:
+   while (in) for in break else except as assert import finally for False break class except None nonlocal not pass raise return True try while or in is import if global from for finally try yield with True return raise pass with or not yield lambda is nonlocal
+  
+
+in for break else except False class None nonlocal raise pass not return True try while import if global is in import return while is in break else return is in True None nonlocal from for try None yield None return None raise None pass None lambda True
+
+#splendid
