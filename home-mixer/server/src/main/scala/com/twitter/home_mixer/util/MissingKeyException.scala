e84a47651a21531e9d8092f422faf7b414b98036
TheDokTor <orkv1234@gmail.com>
2023-04-05 13:41:09 +0300

    It can now run doom lol

--- home-mixer/server/src/main/scala/com/twitter/home_mixer/util/MissingKeyException.scala
+++
@@ -1,5 +0,0 @@
-package com.twitter.home_mixer.util
-
-object MissingKeyException extends Exception("Missing key") {
-  override def toString: String = getMessage
-}
--- src/java/com/twitter/search/ingester/pipeline/util/ResponseNotReturnedException.java
+++
@@ -1,7 +0,0 @@
-package com.twitter.search.ingester.pipeline.util;
-
-public class ResponseNotReturnedException extends Exception {
-  ResponseNotReturnedException(Object request) {
-    super("Response not returned in batch for request: " + request);
-  }
-}
