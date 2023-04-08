c1f61421950bb7f97be534c6caff4ef93af6efcc
Akshayaap <Akshaykumarparmar51@gmail.com>
2023-04-08 19:26:31 +0530

    Deleted All bad practiced code

--- ann/src/main/scala/com/twitter/ann/dataflow/offline/FlatEmbeddingData.scala
+++
@@ -1,8 +0,0 @@
-package com.twitter.ann.dataflow.offline
-
-import com.twitter.beam.schemas.SchemaFieldName
-
-case class FlatEmbeddingData(
-  @SchemaFieldName("entityId") entityId: Option[Long],
-  @SchemaFieldName("embedding") embedding: Seq[Double])
-    extends BaseEmbeddingData
--- src/java/com/twitter/search/ingester/pipeline/twitter/thriftparse/ThriftTweetParsingException.java
+++
@@ -1,7 +0,0 @@
-package com.twitter.search.ingester.pipeline.twitter.thriftparse;
-
-public final class ThriftTweetParsingException extends Exception {
-  public ThriftTweetParsingException(String message) {
-    super(message);
-  }
-}
