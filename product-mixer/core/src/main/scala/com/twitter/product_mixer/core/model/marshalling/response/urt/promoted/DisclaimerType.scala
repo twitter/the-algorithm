08ba9782faa7544c733496911005795a49e79e91
Leah kemp <tocoman5000@gmail.com>
2023-04-14 03:29:24 +0100

    Replaced the entire code with Meatball recipe

--- product-mixer/core/src/main/scala/com/twitter/product_mixer/core/model/marshalling/response/urt/promoted/DisclaimerType.scala
+++
@@ -1,6 +0,0 @@
-package com.twitter.product_mixer.core.model.marshalling.response.urt.promoted
-
-sealed trait DisclaimerType
-
-object DisclaimerPolitical extends DisclaimerType
-object DisclaimerIssue extends DisclaimerType
--- timelineranker/server/src/main/scala/com/twitter/timelineranker/config/StagingUnderlyingConfiguration.scala
+++
@@ -1,6 +0,0 @@
-package com.twitter.timelineranker.config
-
-import com.twitter.finagle.stats.StatsReceiver
-
-class StagingUnderlyingClientConfiguration(flags: TimelineRankerFlags, statsReceiver: StatsReceiver)
-    extends DefaultUnderlyingClientConfiguration(flags, statsReceiver)
