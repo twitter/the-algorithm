510b66c84839dab1c5d7053776cfd1ecdec977f3
dogemanttv <80775876+dogemanttv@users.noreply.github.com>
2024-01-10 17:07:28 -0600

    Delete home-mixer directory

--- home-mixer/server/src/main/scala/com/twitter/home_mixer/model/request/HomeMixerDebugOptions.scala
+++
@@ -1,8 +0,0 @@
-package com.twitter.home_mixer.model.request
-
-import com.twitter.product_mixer.core.model.marshalling.request.DebugOptions
-import com.twitter.util.Time
-
-case class HomeMixerDebugOptions(
-  override val requestTimeOverride: Option[Time])
-    extends DebugOptions
