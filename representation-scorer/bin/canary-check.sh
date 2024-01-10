921e154befb5c8f1a0fd68f3d4f17a1c9fb9eaf9
dogemanttv <80775876+dogemanttv@users.noreply.github.com>
2024-01-10 17:08:21 -0600

    Delete representation-scorer directory

--- representation-scorer/bin/canary-check.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-export CANARY_CHECK_ROLE="representation-scorer"
-export CANARY_CHECK_NAME="representation-scorer"
-export CANARY_CHECK_INSTANCES="0-19"
-
-python3 relevance-platform/tools/canary_check.py "$@"
-
