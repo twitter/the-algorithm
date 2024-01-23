23cec1253386034a1771153ddb158658df18c99c
Ari Archer <ari.web.xyz@gmail.com>
2024-01-23 19:11:50 +0200

    [docx] split commit for file 3000
    Signed-off-by: Ari Archer <ari.web.xyz@gmail.com>

--- product-mixer/core/src/main/scala/com/twitter/product_mixer/core/model/marshalling/response/urt/metadata/Badge.scala
+++
@@ -1,8 +0,0 @@
-package com.twitter.product_mixer.core.model.marshalling.response.urt.metadata
-
-import com.twitter.product_mixer.core.model.marshalling.response.urt.color.RosettaColor
-
-case class Badge(
-  text: Option[String],
-  textColorName: Option[RosettaColor],
-  backgroundColorName: Option[RosettaColor])
--- product-mixer/core/src/main/scala/com/twitter/product_mixer/core/model/marshalling/response/urt/metadata/PinnableEntry.scala
+++
@@ -1,5 +0,0 @@
-package com.twitter.product_mixer.core.model.marshalling.response.urt.metadata
-
-trait PinnableEntry {
-  def isPinned: Option[Boolean] = None
-}
