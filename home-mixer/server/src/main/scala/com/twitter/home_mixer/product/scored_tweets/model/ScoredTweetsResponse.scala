65c3a3fe906c79282874c717b93586d403536de8
Ari Archer <ari.web.xyz@gmail.com>
2024-01-23 19:08:30 +0200

    [docx] split commit for file 1800
    Signed-off-by: Ari Archer <ari.web.xyz@gmail.com>

--- home-mixer/server/src/main/scala/com/twitter/home_mixer/product/scored_tweets/model/ScoredTweetsResponse.scala
+++
@@ -1,6 +0,0 @@
-package com.twitter.home_mixer.product.scored_tweets.model
-
-import com.twitter.product_mixer.core.model.common.presentation.CandidateWithDetails
-import com.twitter.product_mixer.core.model.marshalling.HasMarshalling
-
-case class ScoredTweetsResponse(scoredTweets: Seq[CandidateWithDetails]) extends HasMarshalling
