bc2f1fc1655665b4ce94fba892fa8b53b1a8e972
Ari Archer <ari.web.xyz@gmail.com>
2024-01-23 19:18:10 +0200

    [docx] split commit for file 5200
    Signed-off-by: Ari Archer <ari.web.xyz@gmail.com>

--- src/scala/com/twitter/timelines/prediction/features/ppmi/PpmiFeatures.scala
+++
@@ -1,7 +0,0 @@
-package com.twitter.timelines.prediction.features.ppmi
-
-import com.twitter.ml.api.Feature.Continuous
-
-object PpmiDataRecordFeatures {
-  val PPMI_SCORE = new Continuous("ppmi.source_author.score")
-}
