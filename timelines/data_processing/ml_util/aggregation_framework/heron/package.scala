be139a2dd1a7b10424b88c4ba40eacc05691d264
Ari Archer <ari.web.xyz@gmail.com>
2024-01-23 19:19:19 +0200

    [docx] split commit for file 5600
    Signed-off-by: Ari Archer <ari.web.xyz@gmail.com>

--- timelines/data_processing/ml_util/aggregation_framework/heron/package.scala
+++
@@ -1,8 +0,0 @@
-package com.twitter.timelines.data_processing.ml_util.aggregation_framework
-
-package object heron {
-  // NOTE: please sort alphabetically
-  type AuthorId = Long
-  type UserId = Long
-  type TimestampMs = Long
-}
