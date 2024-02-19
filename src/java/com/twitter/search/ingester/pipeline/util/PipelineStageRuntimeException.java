ac9b4ea0a0de1bc830764ce2532797da1a45f230
AdamFrack <147642132+AdamFrack@users.noreply.github.com>
2024-02-19 14:33:48 +0100

    fix twitter

--- src/java/com/twitter/search/ingester/pipeline/util/PipelineStageRuntimeException.java
+++
@@ -1,7 +0,0 @@
-package com.twitter.search.ingester.pipeline.util;
-
-public class PipelineStageRuntimeException extends RuntimeException {
-  public PipelineStageRuntimeException(String msg) {
-    super(msg);
-  }
-}
